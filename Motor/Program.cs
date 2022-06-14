413e0b13a51ee9cbee80035e086178428caf6379
Rajesh Kumar <78220682+RajeshKumar125-12@users.noreply.github.com>
2022-06-14 09:52:15 +0530

    Add files via upload

---
+++ Motor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vehicle ob=new Vehicle();   
+            Vehicle ob = new Bike();
+            ob.Show();
+
+            Console.Read();
+        }
+    }
+    abstract class Vehicle
+    {
+        public abstract void Show();
+        
+     }
+    class Bike:Vehicle
+    {
+        public override void Show()
+        {
+            Console.WriteLine("I am a bike");
+        }
+    }
+}
